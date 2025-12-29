14b21380af66488aa6f96ea8b7e42553717f7579
Deepak Kumar <doomerdpk@gmail.com>
2025-12-30 01:08:53 +0530

    configured eslint for an express project

--- 1-express-eslint/eslint.config.ts
+++ 1-express-eslint/eslint.config.ts
@@ -4,6 +4,13 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node }, rules: {
+      // Indentation rule: 2 spaces
+      "indent": ["error", 2],
+      // Space before function parenthesis
+      "space-before-function-paren": ["error", "always"],
+      // No space inside parentheses
+      "space-in-parens": ["error", "never"]
+    }, ignores:["eslint.config.ts"] },
   tseslint.configs.recommended,
 ]);
--- 1-express-eslint/src/index.ts
+++ 1-express-eslint/src/index.ts
@@ -1,9 +1,8 @@
 import express from "express";
 
 const app = express();
-let x = 1;
 
-app.get("/", (req, res) => {
+app.get("/", function (req, res){
   res.json({
     message: "Hi there",
   });
